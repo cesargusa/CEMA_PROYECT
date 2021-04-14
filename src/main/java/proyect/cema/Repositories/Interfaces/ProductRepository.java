0b7c0977e40c4a6c608d4ea8f113755e8c8ebc4b
Mario <gutierrezmontoriomario@gmail.com>
2021-04-14 11:18:35 +0200

    He creado la entidad Product con su constructor y variables luego un product services donde metemos el GetAll creamos otro DTO con product donde almacenamos el constructor y los getter y setters y finalmente el product controller

---
+++ src/main/java/proyect/cema/Repositories/Interfaces/ProductRepository.java
@@ -0,0 +1,9 @@
+package proyect.cema.Repositories.Interfaces;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import proyect.cema.Repositories.Entities.ProductEntity;
+import proyect.cema.Repositories.Entities.UsuarioEntity;
+
+public interface ProductRepository extends JpaRepository<ProductEntity,Long>{
+    
+}
